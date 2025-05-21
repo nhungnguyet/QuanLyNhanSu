1dad239f48f99d8e1acf242b640a4e0742e96c94
CamTien-009 <phamthicamtienqn2020@gmail.com>
2025-05-21 14:09:06 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace QLNS_CSDL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btThoat = new System.Windows.Forms.Button();
+            this.btdangnhap = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txbmatkhau = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txbtendangnhap = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btThoat);
+            this.panel1.Controls.Add(this.btdangnhap);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(584, 217);
+            this.panel1.TabIndex = 0;
+            // 
+            // btThoat
+            // 
+            this.btThoat.Location = new System.Drawing.Point(318, 170);
+            this.btThoat.Name = "btThoat";
+            this.btThoat.Size = new System.Drawing.Size(91, 30);
+            this.btThoat.TabIndex = 3;
+            this.btThoat.Text = "Thoát";
+            this.btThoat.UseVisualStyleBackColor = true;
+            this.btThoat.Click += new System.EventHandler(this.btThoat_Click);
+            // 
+            // btdangnhap
+            // 
+            this.btdangnhap.Location = new System.Drawing.Point(430, 170);
+            this.btdangnhap.Name = "btdangnhap";
+            this.btdangnhap.Size = new System.Drawing.Size(91, 30);
+            this.btdangnhap.TabIndex = 2;
+            this.btdangnhap.Text = "Đăng nhập";
+            this.btdangnhap.UseVisualStyleBackColor = true;
+            this.btdangnhap.Click += new System.EventHandler(this.btdangnhap_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.txbmatkhau);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Location = new System.Drawing.Point(3, 79);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(578, 70);
+            this.panel3.TabIndex = 1;
+            // 
+            // txbmatkhau
+            // 
+            this.txbmatkhau.Location = new System.Drawing.Point(232, 25);
+            this.txbmatkhau.Name = "txbmatkhau";
+            this.txbmatkhau.Size = new System.Drawing.Size(280, 22);
+            this.txbmatkhau.TabIndex = 1;
+            this.txbmatkhau.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mật khẩu:";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txbtendangnhap);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(578, 70);
+            this.panel2.TabIndex = 0;
+            // 
+            // txbtendangnhap
+            // 
+            this.txbtendangnhap.Location = new System.Drawing.Point(232, 25);
+            this.txbtendangnhap.Name = "txbtendangnhap";
+            this.txbtendangnhap.Size = new System.Drawing.Size(280, 22);
+            this.txbtendangnhap.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên đăng nhập:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 239);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng nhập";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txbmatkhau;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbtendangnhap;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btThoat;
+        private System.Windows.Forms.Button btdangnhap;
+    }
+}
+
---
+++ fQLNS.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLNS_CSDL
+{
+    public partial class fQLNS : Form
+    {
+        public fQLNS()
+        {
+            InitializeComponent();
+            
+
+        }
+
+ 
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void fQLNS_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void tiềnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void buttLuu_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bạn đã lưu thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void buttHUy_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBNLMKM_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btLuu_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bạn đã lưu thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btHUy_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
