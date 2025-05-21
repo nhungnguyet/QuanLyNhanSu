1ec81698c12af85ce438bcd68fc7652552ff3c1b
CamTien-009 <phamthicamtienqn2020@gmail.com>
2025-05-21 22:12:55 +0700

    Add files via upload

---
+++ fHeThong.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace QLNS_CSDL
+{
+    partial class fHeThong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbmkcu = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txbMKMoi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txtMLMKC = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttLuu = new System.Windows.Forms.Button();
+            this.buttHuy = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.texthienthitep = new System.Windows.Forms.TextBox();
+            this.butchuatep = new System.Windows.Forms.Button();
+            this.buttsaoluu = new System.Windows.Forms.Button();
+            this.bttdong = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl1.Location = new System.Drawing.Point(3, 4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1225, 602);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.buttHuy);
+            this.tabPage1.Controls.Add(this.buttLuu);
+            this.tabPage1.Controls.Add(this.panel3);
+            this.tabPage1.Controls.Add(this.panel2);
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 37);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1217, 561);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Đổi mật khẩu";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.panel4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 37);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1217, 561);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Sao lưu dữ liệu";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txbmkcu);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(319, 88);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(671, 63);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mật khẩu cũ:";
+            // 
+            // txbmkcu
+            // 
+            this.txbmkcu.Location = new System.Drawing.Point(235, 19);
+            this.txbmkcu.Name = "txbmkcu";
+            this.txbmkcu.Size = new System.Drawing.Size(365, 34);
+            this.txbmkcu.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txbMKMoi);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(319, 171);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(671, 63);
+            this.panel2.TabIndex = 1;
+            // 
+            // txbMKMoi
+            // 
+            this.txbMKMoi.Location = new System.Drawing.Point(235, 19);
+            this.txbMKMoi.Name = "txbMKMoi";
+            this.txbMKMoi.Size = new System.Drawing.Size(365, 34);
+            this.txbMKMoi.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 22);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mật khẩu mới:";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.txtMLMKC);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Location = new System.Drawing.Point(319, 254);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(671, 63);
+            this.panel3.TabIndex = 2;
+            // 
+            // txtMLMKC
+            // 
+            this.txtMLMKC.Location = new System.Drawing.Point(235, 19);
+            this.txtMLMKC.Name = "txtMLMKC";
+            this.txtMLMKC.Size = new System.Drawing.Size(365, 34);
+            this.txtMLMKC.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(34, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(183, 22);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Nhập lại mật khẩu cũ:";
+            // 
+            // buttLuu
+            // 
+            this.buttLuu.Location = new System.Drawing.Point(727, 374);
+            this.buttLuu.Name = "buttLuu";
+            this.buttLuu.Size = new System.Drawing.Size(100, 34);
+            this.buttLuu.TabIndex = 4;
+            this.buttLuu.Text = "Lưu";
+            this.buttLuu.UseVisualStyleBackColor = true;
+            this.buttLuu.Click += new System.EventHandler(this.buttLuu_Click);
+            // 
+            // buttHuy
+            // 
+            this.buttHuy.Location = new System.Drawing.Point(922, 374);
+            this.buttHuy.Name = "buttHuy";
+            this.buttHuy.Size = new System.Drawing.Size(100, 34);
+            this.buttHuy.TabIndex = 5;
+            this.buttHuy.Text = "Hủy";
+            this.buttHuy.UseVisualStyleBackColor = true;
+            this.buttHuy.Click += new System.EventHandler(this.buttHuy_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.bttdong);
+            this.panel4.Controls.Add(this.buttsaoluu);
+            this.panel4.Controls.Add(this.butchuatep);
+            this.panel4.Controls.Add(this.texthienthitep);
+            this.panel4.Controls.Add(this.label5);
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Location = new System.Drawing.Point(115, 26);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(976, 484);
+            this.panel4.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Crimson;
+            this.label4.Location = new System.Drawing.Point(359, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(280, 46);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Sao Lưu Dữ Liệu";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(97, 217);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(170, 28);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Chọn vị trí sao lưu";
+            // 
+            // texthienthitep
+            // 
+            this.texthienthitep.Location = new System.Drawing.Point(356, 211);
+            this.texthienthitep.Name = "texthienthitep";
+            this.texthienthitep.Size = new System.Drawing.Size(350, 34);
+            this.texthienthitep.TabIndex = 1;
+            // 
+            // butchuatep
+            // 
+            this.butchuatep.Location = new System.Drawing.Point(786, 209);
+            this.butchuatep.Name = "butchuatep";
+            this.butchuatep.Size = new System.Drawing.Size(82, 36);
+            this.butchuatep.TabIndex = 0;
+            this.butchuatep.Text = "...";
+            this.butchuatep.UseVisualStyleBackColor = true;
+            // 
+            // buttsaoluu
+            // 
+            this.buttsaoluu.Location = new System.Drawing.Point(606, 326);
+            this.buttsaoluu.Name = "buttsaoluu";
+            this.buttsaoluu.Size = new System.Drawing.Size(100, 37);
+            this.buttsaoluu.TabIndex = 2;
+            this.buttsaoluu.Text = "Sao lưu";
+            this.buttsaoluu.UseVisualStyleBackColor = true;
+            this.buttsaoluu.Click += new System.EventHandler(this.buttsaoluu_Click);
+            // 
+            // bttdong
+            // 
+            this.bttdong.Location = new System.Drawing.Point(768, 326);
+            this.bttdong.Name = "bttdong";
+            this.bttdong.Size = new System.Drawing.Size(100, 37);
+            this.bttdong.TabIndex = 3;
+            this.bttdong.Text = "Đóng";
+            this.bttdong.UseVisualStyleBackColor = true;
+            this.bttdong.Click += new System.EventHandler(this.bttdong_Click);
+            // 
+            // fHeThong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1267, 641);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "fHeThong";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "fHeThong";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txtMLMKC;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txbMKMoi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txbmkcu;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttHuy;
+        private System.Windows.Forms.Button buttLuu;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button bttdong;
+        private System.Windows.Forms.Button buttsaoluu;
+        private System.Windows.Forms.Button butchuatep;
+        private System.Windows.Forms.TextBox texthienthitep;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ fHeThong.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLNS_CSDL
+{
+    public partial class fHeThong : Form
+    {
+        public fHeThong()
+        {
+            InitializeComponent();
+        }
+
+        private void buttLuu_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bạn đã lưu thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void buttHuy_Click(object sender, EventArgs e)
+        {
+            txbmkcu.Text = "";
+            txbMKMoi.Text = "";
+            txtMLMKC.Text = "";
+            // Đưa focus về ô đầu tiên nếu muốn
+            txbmkcu.Focus();
+        }
+
+        private void buttsaoluu_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sao lưu tệp thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void bttdong_Click(object sender, EventArgs e)
+        {
+
+            // Giả sử tabControl tên là tabControl1 và tab "Đổi mật khẩu" là tab đầu tiên
+            tabControl1.SelectedIndex = 0;
+        }
+    }
+    
+}
+
